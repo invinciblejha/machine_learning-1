65de6f72fc4fbe4d982377c8a000f2d774860270
Cristina <acgrama@googlemail.com>
2012-05-11 17:25:24 +0200

    Some more infrastructure in datareader.py

--- datareader.py
+++ datareader.py
@@ -44,8 +44,55 @@ def randomize_inputs(X, y):
     
     return (new_X, new_y)
  
+def parse_input_no_conversion(input_file, custom_delimiter, input_columns, output_column, is_test):
+    ''' This function parses the input data file, performing no conversion on the input values.
+    
+    Args:
+        input_file: The file containing the input data.
+        custom_delimiter: The delimiter used in the input files.
+        input_columns: Which columns in the input data are inputs (X). If input_columns is empty, the data samples have variable length.
+        output_column: Which column in the input data is output value (y).
+        is_test: Set to True if we are parsing input from a test set.
+    
+    Returns:
+        A set (X, y) containing the input data.    
+    '''
+    data_reader = csv.reader(open(input_file, 'rb'), delimiter=custom_delimiter)
+    
+    if not is_test:
+        X = []
+        y = []
+        index = 0
+        for row in data_reader:
+            line_x = [1] # Add the X0=1
+            while '' in row:
+                row.remove("")
+            if input_columns != []:
+                for i in input_columns:
+                    line_x.append(row[i])
+                X.append(line_x)
+                y.append(row[output_column])
+            else:
+                for i in range(len(row)):
+                    line_x.append(row[i])
+                X.append(line_x)
+        
+        (X, y) = randomize_inputs(X, y)
+    else:
+        X = []
+        for row in data_reader:
+            line_x = [1]
+            for i in range(len(row)):
+                line_x.append(row[i])
+            X.append(line_x)
+        
+        y = [0.0] * len(X) # Dummy y
+        (X, y) = randomize_inputs(X, y)
+    
+    return (X, y)
+
 def parse_input(input_file, custom_delimiter, input_columns, output_column, is_test, input_literal_columns, input_label_mapping, output_literal, output_label_mapping):
-    ''' This function parses the input data file.
+    ''' This function parses the input data file and converts literal values using the specified mappings.
     
     Args:
         input_file: The file containing the input data.
@@ -56,7 +103,7 @@ def parse_input(input_file, custom_delimiter, input_columns, output_column, is_t
         input_literal_columns: Which columns in the input data have a literal description and need to be mapped to custom numeric values.
         input_label_mapping: Mapping for input literal columns.
         output_literal: Boolean, shows whether output is literal or numeric.
-        output_label_mapping: Mapping for output literal column. If this is empty and output_literal=True, it means that we have some clustering task where every sample has a specific output -- the outputs will be marked 0, 1, etc.
+        output_label_mapping: Mapping for output literal column.
     
     Returns:
         A set (X, y) containing the input data.    
@@ -79,11 +126,7 @@ def parse_input(input_file, custom_delimiter, input_columns, output_column, is_t
                         line_x.append(float(row[i]))
                 X.append(line_x)
                 if output_literal:
-                    if output_label_mapping != {}:
-                        y.append(float(output_label_mapping[row[output_column]]))
-                    else:
-                        y.append(index)
-                        index += 1
+                    y.append(float(output_label_mapping[row[output_column]]))
                 else:
                     y.append(float(row[output_column]))
             else:
@@ -105,7 +148,7 @@ def parse_input(input_file, custom_delimiter, input_columns, output_column, is_t
     
     return (X, y)
 
-def readInputData(input_file, input_test_file, custom_delimiter, proportion_factor, split, input_columns, output_column, input_literal_columns, input_label_mapping, output_literal, output_label_mapping):
+def readInputData(input_file, input_test_file, convert_literals, custom_delimiter, proportion_factor, split, input_columns, output_column, input_literal_columns, input_label_mapping, output_literal, output_label_mapping):
     ''' Main method for parsing the input data. The input data is expected in CSV format, with a delimiter that can be specified as parameter.
     The method generates a random permutation of the read data to be safe in case the original raw data is nicely ordered.
     It uses the proportion_factor to determine how much data should be for training and how much for testing.
@@ -113,6 +156,7 @@ def readInputData(input_file, input_test_file, custom_delimiter, proportion_fact
     Args:
         input_file: The file containing the input data.
         input_test_file: The file containing the test data (if applicable).
+        convert_literals: If True, the literals in the input files will be converted to numeric values as per the given mappings.
         custom_delimiter: The delimiter used in the input files.
         proportion_factor: If there is no special input_test_file, a percentage of proportion_factor% from the input_file will be used as test data. The samples are randomly selected.
         split: If true, the test data will be taken from input_file. Otherwise, from input_test_file.
@@ -126,7 +170,10 @@ def readInputData(input_file, input_test_file, custom_delimiter, proportion_fact
     Returns:
         A set (train_X, train_y, test_X, test_y) containing training data and test data. The test_y array contains dummy values.
     '''
-    (X, y) = parse_input(input_file, custom_delimiter, input_columns, output_column, False, input_literal_columns, input_label_mapping, output_literal, output_label_mapping)
+    if convert_literals:
+        (X, y) = parse_input(input_file, custom_delimiter, input_columns, output_column, False, input_literal_columns, input_label_mapping, output_literal, output_label_mapping)
+    else:
+        (X, y) = parse_input_no_conversion(input_file, custom_delimiter, input_columns, output_column, False)
     
     if split:
         splice_index = int(len(y) * proportion_factor)
--- plants_kmeans.py
+++ plants_kmeans.py
@@ -5,7 +5,7 @@ Author: AC Grama http://acgrama.blogspot.com
 Date: 05.05.2012
 '''
 
-import csv, datareader, math, mlpy, matplotlib.pyplot as plt, numpy, random, scipy.optimize
+import csv, datareader
 
 if __name__ == "__main__":
     print "Parsing input data..."
@@ -17,11 +17,18 @@ if __name__ == "__main__":
     split = True 
     input_columns = []
     output_column = 0
-    input_literal_columns = [1] * 8
+    input_literal_columns = []
     input_label_mapping = {}
     output_literal = True
     output_label_mapping = {}
-    (train_X, train_y, test_X, test_y) = datareader.readInputData(input_file, input_test_file, custom_delimiter, 
+    (train_X, train_y, test_X, test_y) = datareader.readInputData(input_file, input_test_file, False, custom_delimiter, 
         proportion_factor, split, input_columns, output_column, input_literal_columns, input_label_mapping, output_literal, output_label_mapping)
     print "Parsing complete!\n"
+    
+    print train_X
+    
+    state_abbrev = {'ab':0, 'ak':1, 'ar':2, 'az':3, 'ca':4, 'co':5, 'ct':6, 'de':7, 'dc':8, 'fl ':9, 'ga':10, 'hi':11, 'id':12, 'il':13, 'in':14, 'ia':15, 'ks':16, 'ky':17, 'la':18, 'me':19, 'md':20, 
+    'ma':21, 'mi':22, 'mn':23, 'ms':24, 'mo':25, 'mt':26, 'ne':27, 'nv':28, 'nh':29, 'nj':30, 'nm':31, 'ny':32, 'nc':33, 'nd':34, 'oh':35, 'ok':36, 'or':37, 'pa':38, 'pr':39, 'ri':40, 
+    'sc':41, 'sd':42, 'tn':43, 'tx':44, 'ut':45, 'vt':46, 'va':47, 'vi':48, 'wa':49, 'wv':50, 'wi':51, 'wy':52, 'al':53, 'bc':54, 'mb':55, 'nb':56, 'lb':57, 'nf':58, 'nt':59, 'ns':60, 
+    'nu':61, 'on':62, 'qc':63, 'sk':64, 'yt':65, 'dengl':66, 'fraspm':67}
 
